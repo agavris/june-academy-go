57d86f0157c21736b67d5ce21f31950e523c80e3
agavris <agavris@protonmail.com>
2023-12-23 11:00:34 -0500

    Initial commit

--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
-module github.com/agavris/june-academy-go
+module juneacademy
 
 go 1.20
-
-require (
-	github.com/gocarina/gocsv v0.0.0-20231116093920-b87c2d0e983a // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-)
--- main.go
+++ main.go
@@ -2,21 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/agavris/june-academy-go/src/algorithm/scheduler"
-	"time"
 )
 
-func timer(name string) func() {
-	start := time.Now()
-	return func() {
-		fmt.Println(name, "took", time.Since(start))
-	}
-}
-
 func main() {
-
-	Scheduler := scheduler.NewScheduler()
-	defer timer("main")()
-	Scheduler.Run(10000)
-
+	fmt.Println("Hello World!")
 }
