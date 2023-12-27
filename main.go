214550b9c91b72a3c8d7058011387e269e534a98
agavris <agavris@protonmail.com>
2023-12-27 12:48:42 -0500

    First Commit

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/agavris/june-academy-go/src/algorithm/scheduler"
+	"time"
 )
 
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Println(name, "took", time.Since(start))
+	}
+}
+
 func main() {
-	fmt.Println("Hello World!")
+
+	Scheduler := scheduler.NewScheduler()
+	defer timer("main")()
+	Scheduler.Run(10000)
+
 }
